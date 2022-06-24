e5a02846b14b590ac87b389a74516f7a42c6c7e3
screamuch <peterchudinov@icloud.com>
2022-06-24 00:06:07 -0700

    added note length, cleanup method, new reference midis
    cleaning

--- create_midi.py
+++ create_midi.py
@@ -78,8 +78,6 @@ def transcribe_from_string(audio_string):
             time = (CHUNK/RATE) * frame * BPM/60
             # insert note into midi: track? 0, instrument? 0, pitch, time in beats, 1/16 note, velocity 64
             MyMIDI.addNote(0, 0, pitch, time, 1/4, 64)
-            # print note information
-            print(pitch, time)
         # read next chunk
         buffer = f.readframes(CHUNK)
         frame += 1
@@ -96,30 +94,83 @@ def transcribe_from_string(audio_string):
     
     return midi_file_name
 
-# this function takes care of alignment tool management (because it's so fucked)
+# this helper method takes care of alignment tool management (because it's so fucked)
 def align(user_midi_file_name, reference_midi_file_name):
     # move user midi to alignment folder
-    subp.check_call('mv ' + user_midi_file_name + ' AlignmentTool/' + user_midi_file_name, shell=True)
+    subp.run('mv ' + user_midi_file_name + ' AlignmentTool/' + user_midi_file_name, shell=True)
     # copy reference midi to alignment folder
-    subp.check_call('cp ' + 'reference_midi/' + reference_midi_file_name + ' AlignmentTool/' + reference_midi_file_name, shell=True)
+    subp.run('cp ' + 'reference_midi/' + reference_midi_file_name + ' AlignmentTool/' + reference_midi_file_name, shell=True)
+    # TODO: remove this ugly hack
+    timestamp.sleep(3)
     # change directory to alignment folder and run alignment on user and reference midi
-    subp.check_call('cd AlignmentTool/ && ./MIDIToMIDIAlign.sh ' + user_midi_file_name[:-4] + ' ' + reference_midi_file_name[:-4], shell=True)
+    subp.run('cd AlignmentTool/ && ./MIDIToMIDIAlign.sh ' + reference_midi_file_name[:-4] + ' ' + user_midi_file_name[:-4], shell=True)
+
+# this hmethod converts beat length (0-1) to vexflow friendly units
+def vexflow_length(beat_length):
+    vf_length = 'q'
+    if beat_length >= 0.03124:
+        vf_length = '32'
+    if beat_length >= 0.046874:
+        vf_length = '32d'
+    if beat_length >= 0.0624:
+        vf_length = '16'
+    if beat_length >= 0.09375:
+        vf_length = '16d'
+    if beat_length >= 0.124:
+        vf_length = '8'
+    if beat_length >= 0.1875:
+        vf_length = '8d'
+    if beat_length >= 0.24:
+        vf_length = 'q'
+    if beat_length >= 0.374:
+        vf_length = 'qd'
+    if beat_length >= 0.49:
+        vf_length = 'h'
+    if beat_length >= 0.74:
+        vf_length = 'hd'
+    if beat_length >= 0.99:
+        vf_length = 'w'
+
+# this helper method cleans up the directories from generated clutter
+def clean_up(user_midi_file_name, reference_midi_file_name):
+    # remove wav
+    wav_file_name = user_midi_file_name[:-4] + '.wav'
+    subp.run('rm ' + wav_file_name, shell=True)
+
+    # remove AlignmentTool output
+    user_midi_file_name_base = user_midi_file_name[:-4]
+    reference_midi_file_name_base = reference_midi_file_name[:-4]
+
+    subp.run('rm AlignmentTool/' + user_midi_file_name_base + "_corresp.txt", shell=True)
+    subp.run('rm AlignmentTool/' + user_midi_file_name_base + "_match.txt", shell=True)
+    subp.run('rm AlignmentTool/' + user_midi_file_name_base + "_spr.txt", shell=True)
+    subp.run('rm AlignmentTool/' + user_midi_file_name, shell=True)
+
+    subp.run('rm AlignmentTool/' + reference_midi_file_name_base + "_fmt3x.txt", shell=True)
+    subp.run('rm AlignmentTool/' + reference_midi_file_name_base + "_hmm.txt", shell=True)
+    subp.run('rm AlignmentTool/' + reference_midi_file_name_base + "_spr.txt", shell=True)
+    subp.run('rm AlignmentTool/' + reference_midi_file_name, shell=True)
 
 # right now reference midi file name = None and it WILL crash when executed
 def extract_errors(user_midi_file_name, reference_midi_file_name):
     # run alignment tool
     align(user_midi_file_name, reference_midi_file_name)
     # load tables into pandas
-    corresp_file_name = 'AlignmentTool/' + reference_midi_file_name[:-4] + "_corresp.txt"
+    corresp_file_name = 'AlignmentTool/' + user_midi_file_name[:-4] + "_corresp.txt"
     corresp_header = ["id", "onset_time", "spelled_pitch", "integer_pitch", "onset_velocity", "reference_id",
                 "reference_onset_time", "reference_spelled_pitch", "reference_integer_pitch", "reference_onset_velocity", "blank"]
     corresp_data = pd.read_csv(corresp_file_name, sep='\t', skiprows=[0], names=corresp_header, index_col=0)
 
-    match_file_name = 'AlignmentTool/' + reference_midi_file_name[:-4] + "_match.txt"
+    match_file_name = 'AlignmentTool/' + user_midi_file_name[:-4] + "_match.txt"
     match_header = ["id", "onset_time", "offset_time", "spelled_pitch", "onset_velocity",
                 "offset_velocity", "channel", "match_status", "score_time", "note_id",
                 "error_index", "skip_index"]
     match_data = pd.read_csv(match_file_name, sep='\t', skiprows=[0,1,2,3], names=match_header, index_col=0)[:-2]
+    
+    spr_file_name = 'AlignmentTool/' + reference_midi_file_name[:-4] + "_spr.txt"
+    spr_header = ["id", "onset_time", "offset_time", "spelled_pitch", "onset_velocity", "other_velocity", "null"]
+    spr_data = pd.read_csv(spr_file_name, sep='\t', skiprows=[0], names=spr_header, index_col=0)
+    spr_data['time_diff'] = spr_data.offset_time - spr_data.onset_time
 
     # this has to be a dictionary of all notes - pitches and time (reference + mistakes)
     # return format:
@@ -130,13 +181,11 @@ def extract_errors(user_midi_file_name, reference_midi_file_name):
     #     pitch_integer
     #     pitch_spelled
     #     onset_time -> seconds from start of measure
-    #     length -> note length (like "1/4")
+    #     length -> note length (like "q", vexflow format)
     #     note_type -> "extra, missing, incorrect, reference"
     # ]
 
     # consider creating rests as well
-
-    # code below is untested, need audio from Mohamed and reference midi
 
     reference_bpm = 60 # should come from midi
     reference_timesig_numerator = 4 # should come from midi
@@ -156,22 +205,30 @@ def extract_errors(user_midi_file_name, reference_midi_file_name):
         pitch_integer = row['reference_integer_pitch']
         pitch_spelled = row['reference_spelled_pitch']
         onset_time = row['reference_onset_time']
-        # !!!!! oh fuck we don't have reference offset here so no length ffs
         measure = measure_func(onset_time)
         note_type = "reference"
+        length = None
+
+        # get note length if reference exists
+        if row['reference_id'] != '*':
+            time_length = spr_data.iloc[[row['reference_id']]]["time_diff"].values[0]
+            beat_length = (BPM * reference_timesig_numerator) / 60
+            length = vexflow_length(beat_length)
+
         # process extra notes
         if row['reference_id'] == '*':
             note_type = "extra"
             pitch_integer = row['integer_pitch']
             pitch_spelled = row['spelled_pitch']
             onset_time = row['onset_time']
+            length = 'q'
         # process missing notes
         elif idx == '*':
             note_type = "missing"
         # process incorrect pitch
         elif match_data.iloc[[idx]]["error_index"].values[0] == 1:
             note_type = "incorrect"
-            pitch_played_spelled = match_data.iloc[[idx]]['spelled_pitch']
+            pitch_played_spelled = match_data.iloc[[idx]]['spelled_pitch'].values[0]
 
         note = {
             'measure': measure,
@@ -179,13 +236,14 @@ def extract_errors(user_midi_file_name, reference_midi_file_name):
             'pitch_spelled': pitch_spelled,
             'pitch_played_spelled': pitch_played_spelled,
             'onset_time': onset_time,
-            'length': None,
+            'length': length,
             'note_type': note_type
         }
 
         performance_data['notes'].append(note)
 
-    # it would be good to remove generated txt files and the midi files here
+    # remove generated file garbage
+    clean_up(user_midi_file_name, reference_midi_file_name)
 
     # sort notes of dictionary by onset time and pitch
     performance_data['notes'].sort(key=lambda x: (x['onset_time'], x['pitch_integer']))
